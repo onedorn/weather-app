b1b15b1347b883bcaa8acba4e5d547f907cd164c
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-04-19 00:15:00 +0300

    Create 2 request to AccuWhether APIs, fetch a location, after the weather condition.

---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key = 'S7EogXA5Ei47wdd6jb4PoHIqQfSIiZiD';
+
+
+// First API request, searching for a location
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+getCity('Amsterdam').then(data => {
+        return getWeather(data.Key);
+    }).then(data => {
+        console.log(data);
+    }).catch(err => console.log(err));
+
+// Second API request, searching for a condition at that HTMLTextAreaElement, for weater condition in my case
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+}
